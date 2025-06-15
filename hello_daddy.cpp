e1f98f7deeca0703dc24d77cba6d14052f71fe33
roble656 <robyay8@gmail.com>
2025-06-15 13:44:04 +0300

    up date

--- hello_daddy.cpp
+++ hello_daddy.cpp
@@ -3,16 +3,8 @@
 using namespce std;
 int main()
 {
-    cout<<" hello daddy ";
-    cin>>hello;
-<<<<<<< HEAD
-    cout<<"hello mam";
-=======
-    cout<<" hello ";
->>>>>>> c226f2b0b7e39c077577f7dd4261366c9bf920cf
-    
-    
-    
-    
+    cout<<" hello daddy";
+    return 0;
+   
     
 }
